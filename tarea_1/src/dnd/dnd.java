e68d28af8d004311f770b5b2a29b42b7bc156124
Leonstadt <alfredo.ordonez@tecnologicoliceocristiano.edu.ec>
2024-12-22 11:23:01 -0500

    Add files via upload

---
+++ tarea_1/src/dnd/dnd.java
@@ -0,0 +1,26 @@
+package dnd;
+
+public class dnd {
+String edicion;
+int numero_jugadores;
+String dungeon_master;
+int nivel_maximo;
+int nivel_inicial;
+boolean one_shot;
+boolean requiere_miniaturas;
+String setting;
+int cantidad_dados;
+int tiempo_promedio_sesion;
+String genero;
+boolean usa_mapa;
+String reglas_caseras;
+boolean personajes_personalizados;
+String expansiones;
+int cantidad_clases;
+int cantidad_razas;
+boolean requiere_manual;
+String aplicaciones_complementarias;
+boolean combate_rapido;
+
+
+}
